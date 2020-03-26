749e5d6eede38d40d5fec00dea6832f4525dc44c
FlimothyCrow <timshogren@gmail.com>
2020-03-26 12:30:28 -0600

    defined map2 var var var var

--- src/Horse.js
+++ src/Horse.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import fizzBuzz from './fizzBuzz.js'
 
+function map2(list, functionToEx) {
+  var listToReturn = []
+  for (var i in list) { // slams head
+    listToReturn.push(functionToEx(i))
+  }
+  return listToReturn
+}
 
 function Horse() {
-  var test = fizzBuzz();
-  console.log(test)
+  var fizzBussList = fizzBuzz();
+  // map calls the function for you
+
+   // <li> to HTML and {} into JS
+  /*
+  var things2 = []
+  var things3 = map2(fizzBussList, x =>  (<li>{x}y{x}</li>))
+  for (var x in fizzBussList) {
+    things2.push(<li>{x}y{x}</li>)
+  }  // content of things2 are react fragments with HTML list items
+  */
   return (
     <ul>
-      {
-        test.map(x => (<li>{x}</li>) )
+      { // JS
+        fizzBussList.map(x =>  (<li><b>{x}y</b> {x}</li>) )
       }
     </ul> // this is called JSX
-  );
+  ); //program converts to HTML whenever it reads HTML tags
 }
 // logic with JS
 // presentation with JSX
