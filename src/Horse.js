3212b265db5fd460020ada2436e10fcf44109899
FlimothyCrow <timshogren@gmail.com>
2020-03-27 09:25:26 -0600

    mmm horse

--- src/Horse.js
+++ src/Horse.js
@@ -1,34 +1,42 @@
 import React from 'react';
 import fizzBuzz from './fizzBuzz.js'
 
-function map2(list, functionToEx) {
-  var listToReturn = []
-  for (var i in list) { // slams head
-    listToReturn.push(functionToEx(i))
+
+
+class Horse extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {timesClicked:0} ; // "this" adds new class / memeber variable
   }
-  return listToReturn
-}
-
-function Horse() {
-  var fizzBussList = fizzBuzz();
-  // map calls the function for you
-
-   // <li> to HTML and {} into JS
-  /*
-  var things2 = []
-  var things3 = map2(fizzBussList, x =>  (<li>{x}y{x}</li>))
-  for (var x in fizzBussList) {
-    things2.push(<li>{x}y{x}</li>)
-  }  // content of things2 are react fragments with HTML list items
-  */
-  return (
-    <ul>
-      { // JS
-        fizzBussList.map(x =>  (<li><b>{x}y</b> {x}</li>) )
+  increment() { // you don't need "function" because it's within a class
+    this.setState({timesClicked:this.state.timesClicked + 1}) // study setState
+  }
+  render() { // every time state changes
+    var fizzBussList = fizzBuzz(); // NEVER CALL SET.STATE INSIDE RENDER
+    var message = "" ; // rendering is expensive, don't overdo it
+    if (this.state.timesClicked !== 0) {
+      message = "Times clicked " + this.state.timesClicked
+    } // react can't return a list of fragments
+    return (
+      <>
+      {message // no multiline statements in here, only single expressions
       }
-    </ul> // this is called JSX
-  ); //program converts to HTML whenever it reads HTML tags
+      <ul>
+        { // JS
+          fizzBussList.map(x => // thiccboi arrow doesn't need var
+            (<li>
+              <button onClick={() => this.increment()}>done</button> // look in "this.class"
+              <b> {x}y</b> {x}
+            </li>) ) // single statement can be in multiple lines
+        }
+      </ul>
+      </>
+    );// this is called JSX
+  } //program converts to HTML whenever it reads HTML tags
 }
 // logic with JS
 // presentation with JSX
+// call set state
+// initialize state with constructor
+// call render to print html
 export default Horse; // creates public access to this function
