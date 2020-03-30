f7c8f1cb6e10761940c1355159a4085184b03c5a
FlimothyCrow <timshogren@gmail.com>
2020-03-30 10:25:07 -0600

    duplication removal

--- src/fizzBuzzPrinter.js
+++ src/fizzBuzzPrinter.js
@@ -8,14 +8,25 @@ class FizzBuzzPrinter extends React.Component {
           fizzBussList:fizzBuzz()
         };
   }
+  removeTarget(index){
+    delete this.state.fizzBussList[index] // delete mutates without a return
+    this.setState({fizzBussList:this.state.fizzBussList}) // render triggers from setState
+  }
+
   render(){
     return(
       <ul>
-        {this.state.fizzBussList.map(i => {
-          if (isNaN(i)){
-            return (<li style={{color:"blue"}}>{i}</li>)
+        {this.state.fizzBussList.map((iter, index) => { // iter === 1, index === 0th
+          var color = "green"
+          var buttonText = "X"
+
+          if (isNaN(iter)){
+            color = "red"
+            buttonText = "S"
           }
-          return (<li>{i}</li>)
+          return (<li style={{color:color}}>
+            <button onClick={() => this.removeTarget(index)}>{buttonText}</button>
+            {iter}</li>)
         }
         )}
       </ul>
