d2a52a4cf4a278886c5ae605332631cc91f62447
FlimothyCrow <timshogren@gmail.com>
2020-05-22 18:04:45 -0600

    added input

--- src/columnKata.js
+++ src/columnKata.js
@@ -3,32 +3,47 @@ import React from 'react';
 class ColumnKata extends React.Component {
   constructor(props){
         super(props);
-        this.state = {newItem:"",
+        this.state = {newObject:{date:"", description:""},
                       tableOfItems:[]};
   }
   addToList(input){
-    var newTable = this.state.tableOfItems.concat([this.state.newItem])
-    this.setState({tableOfItems:newTable, newItem:""})
+    var newTable = this.state.tableOfItems.concat([input])
+    this.setState({tableOfItems:newTable, newObject:{date:"", description:""}})
   }
 
   render(){
+    console.log("state", this.state)
     return(
     <>
-    <input type="text" value={this.state.newItem}
-      onChange={(event) => this.setState({newItem:event.target.value})}/>
-
-    <button onClick={() => this.addToList(this.state.newItem)}>Add</button>
+    <input type="text" value={this.state.newObject.date}
+      onChange={(event) => this.setState({newObject:{date:event.target.value,
+                                                    description:this.state.newObject.description}})}/>
+    <input type="text" value={this.state.newObject.description}
+      onChange={(event) => this.setState({newObject:{description:event.target.value,
+                                                    date:this.state.newObject.date}})}/>
+    <button onClick={() => this.addToList(this.state.newObject)}>Add</button>
     <table>
+      <tbody>
       <tr>
         <th>date</th>
         <th>description</th>
       </tr>
       {this.state.tableOfItems.map((iter, index) => { // iter === 1, index === 0th
         return (<tr key={index}>
-                  {iter}
+                  <td>{iter.date}</td>
+                  <td>{iter.description}</td>
                </tr>)})}
+      </tbody>
     </table>
     </>
   )}
 }
 export default ColumnKata
+// newObject (description:"", date:"")
+// tableOfItems should be listOfObjects
+// line 26 {iter.date} {iter.description}
+// second input but single button to add both
+// inputs on 17 and 20 generate object{newObject}
+// button on 23 feeds newObject into addToList call
+// render function is always looping
+// setState automatically calls a reRender
