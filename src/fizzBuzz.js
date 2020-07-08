e24cccf25281a9e7d5f9163231a492e16c61743b
FlimothyCrow <timshogren@gmail.com>
2020-07-08 12:10:09 -0600

    leet version of fizzBuzz

--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,26 +1,34 @@
 
 
-function nuFizz(upperLimit){
-  var range = []
+export function nuFizz(upperLimit){
+  var rangeList = []
   for (var i = 1; i <= upperLimit; i++){
-    range.push(i);
+    rangeList.push(i)
   }
-  return range.map(element => {
-    if (element % 2 === 0 && element % 3 === 0){
+  return rangeList.map(number => {
+    if (number % 2 === 0 && number % 3 === 0){
       return "fizzBuzz"
     }
-    else if (element % 3 === 0){
-      return "buzz"
-    }
-    else if (element % 2 === 0){
+    else if (number % 2 === 0){
       return "fizz"
     }
-    else return element
+    else if (number % 3 === 0){
+      return "buzz"
+    }
+    else return number
   })
 }
 
 
+function spicyFizz(upperLimit){
+  var newArray = []
+  for (var i = 1; i <= upperLimit; i++){
+    newArray.push(((i % 3)? (i % 2)? i:'fizz':(i % 2) ?'buzz':'fizzBuzz'))
+  }
+  return newArray
+}
 
-export default nuFizz
 
-// interview pep
+
+
+export default spicyFizz
--- src/fizzBuzz.test.js
+++ src/fizzBuzz.test.js
@@ -1,10 +1,24 @@
 import nuFizz from "./fizzBuzz.js"
+import spicyFizz from "./fizzBuzz.js"
 import deepEqual from 'deepequal'
 
-test('nuFizz0', () => {
-  var upperLimit = 6
+
+
+/*test('nuFizz0', () => {
+  var upperLimit = 9
   var x = nuFizz(upperLimit);
   
   console.log(x)
-  expect(deepEqual(x, [1, "fizz", "buzz", "fizz", 5, "fizzBuzz"])).toBe(true)
+  expect(deepEqual(x, [1, "fizz", "buzz", "fizz", 5, "fizzBuzz",
+                      7, "fizz", "buzz"])).toBe(true)
+});
+*/
+// ----------------
+test('spicyFizz0', () => {
+  var upperLimit = 9
+  var x = spicyFizz(upperLimit);
+  
+  console.log(x)
+  expect(deepEqual(x, [1, "fizz", "buzz", "fizz", 5, "fizzBuzz",
+                      7, "fizz", "buzz"])).toBe(true)
 });
