6bfaa22430e264c9bdf3c58dd0dfaa2c7f2977e1
FlimothyCrow <timshogren@gmail.com>
2020-07-12 19:52:14 -0600

    testing for flask

--- src/HelloWorld.jsx
+++ src/HelloWorld.jsx
@@ -3,15 +3,21 @@ import { render } from "@testing-library/react";
 
 class HelloWorld extends React.Component {
   constructor(props) {
-    // consturctor arranges mise en place
     super(props);
-    this.state = [];
-    // "this" adds new class / memeber variable
+    this.state = {};
   }
-  //this.state.fizzBussList = fizzBuzz(); // NEVER CALL SET.STATE INSIDE RENDER
-  render(){
-      return <span>hello, world!</span>
+  componentDidMount() {
+      fetch("http://localhost:3000/new")
+      .then(result => {
+          return result.json()
+      })
+      .then((result) => {
+          console.log(result)
+      })
+  }
+  render() {
+    return <span>hello, world!</span>;
   }
 }
 
-export default HelloWorld
+export default HelloWorld;
