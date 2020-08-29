a8ba09c21918a3067377eb403fb567c0ceae5a85
FlimothyCrow <timshogren@gmail.com>
2020-08-29 15:31:22 -0600

    defined drawHand and test

--- src/nu_poker.js
+++ src/nu_poker.js
@@ -9,3 +9,26 @@ export function deckMaker() {
     });
   });
 }
+
+export function drawHand(deck){
+  //return _.pullAt(_.shuffle(deck), _.range(5)) 
+  // to do, secret third spicy option for maintainabilito
+  return _.pullAt(deck, _.range(5).map(x => _.random(0, deck.length)))
+}
+
+export function addFive(x){
+  x = x + 5
+  return x
+}
+
+// pass by reference does NOT clone object
+// pass by value clones
+// mutation by reference is NOT GOOD
+// maintainability
+
+
+
+export function addSix(x, z){
+  x.push(6)
+  z.push(6)
+}
--- src/nu_poker.test.js
+++ src/nu_poker.test.js
@@ -1,13 +1,38 @@
-import {deckMaker} from './nu_poker.js'
+import {deckMaker, drawHand, addFive, addSix} from './nu_poker.js'
 import deepEqual from 'deepequal'
 
 
 
 test('deckMaker', ()=>{
   var deckObject = deckMaker()
-  console.log(deckObject)
+  //console.log(deckObject)
   expect(deepEqual(deckObject[0], {face:"a", suit:"d"})).toBe(true)
   expect(deepEqual(deckObject[8], {face:"9", suit:"d"})).toBe(true)
   expect(deepEqual(deckObject[13], {face:"a", suit:"c"})).toBe(true)
   expect(deepEqual(deckObject.length, 52)).toBe(true)
+});
+
+test('drawHand', ()=>{
+  var deck = deckMaker()
+  var hand = drawHand(deck)
+  //console.log(hand)
+  expect(hand.length).toBe(5)
+  expect(deck.length).toBe(47)
+});
+
+
+test('passByValue', ()=>{
+  var y = 10
+  var ret = addFive(y)
+  expect(ret).toBe(15)
+  expect(y).toBe(10)
+});
+
+test('passByRef', ()=>{
+  var y = [10]
+  var z = [11]
+  addSix(y, z)
+  expect(y).toEqual([10, 6])
+  expect(z).toEqual([11, 6])
+  
 });
