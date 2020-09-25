bc2d67ff01baa95c6a032c6ccce5c51e4fae8b1d
FlimothyCrow <timshogren@gmail.com>
2020-09-25 12:26:12 -0600

    defined sumNestedArrays and test

--- src/hackerRank.js
+++ src/hackerRank.js
@@ -1,13 +1,23 @@
-
-
-export function repeatingString(smallString, firstN, toCount){
-    var timesToRepeat = Math.floor(firstN / smallString.length) + firstN % smallString.length
-    var longString = smallString.repeat(timesToRepeat)
-    var counter = 0
-    for (var char = 0; char < firstN; char++){
-        if (longString[char] === toCount){
-            counter += 1
-        }
+export function repeatingString(smallString, firstN, toCount) {
+  var timesToRepeat =
+    Math.floor(firstN / smallString.length) + (firstN % smallString.length);
+  var longString = smallString.repeat(timesToRepeat);
+  var counter = 0;
+  for (var char = 0; char < firstN; char++) {
+    if (longString[char] === toCount) {
+      counter += 1;
     }
-    return counter
+  }
+  return counter;
 }
+
+export function sumNestedArrays(nestedArrays) {
+  var accumulate = 0
+  for (var array = 0; array < nestedArrays.length; array++) {
+    for (var int of nestedArrays[array]) {
+      accumulate += int
+    }
+  }
+  return accumulate
+}
+
--- src/hackerRank.test.js
+++ src/hackerRank.test.js
@@ -1,5 +1,6 @@
 import {
-repeatingString
+repeatingString,
+sumNestedArrays
 } from "./hackerRank.js";
 
 test("repeatingString 0", () => {
@@ -15,5 +16,15 @@ test("repeatingString 0", () => {
   test("repeatingString 2", () => {
     var results = repeatingString("qwerty", 19, "q")
     expect(results).toEqual(4);
+  });
+// ---------------------------------------------------      
+  test("sumNestedArrays 0", () => {
+    var nestedArrays = [
+      [1, 3, 5, 2, 10, 238],
+      [55, 9, 3, 2],
+      [1, 9, 2, 39, 11]
+    ]
+    var results = sumNestedArrays(nestedArrays)
+    expect(results).toEqual(390);
   });
       
