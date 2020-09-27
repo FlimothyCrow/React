0501c2203ed8bb1c24b781ee230f9a78e7b5b4fd
FlimothyCrow <timshogren@gmail.com>
2020-09-27 12:41:39 -0600

    defined lengthNestedArrays

--- src/hackerRank.js
+++ src/hackerRank.js
@@ -11,13 +11,67 @@ export function repeatingString(smallString, firstN, toCount) {
   return counter;
 }
 
-export function sumNestedArrays(nestedArrays) {
-  var accumulate = 0
-  for (var array = 0; array < nestedArrays.length; array++) {
-    for (var int of nestedArrays[array]) {
-      accumulate += int
+
+export function reduceNestedArrays(nestedArrays) {
+  if (nestedArrays.length === 0) {
+    return 0;
+  }
+  let sum = 0;
+  for (var element of nestedArrays) {
+    if (Array.isArray(element)) {
+      // if there's more work to do
+      sum += reduceNestedArrays(element);
+    } else {
+      sum += element;
     }
   }
-  return accumulate
+  return sum;
+}
+// .reduce() won't work on nested arrays
+
+export function reduceListOne(input) {
+  if (input === null) {
+    return 0;
+  } // implicitly defines input.length variables
+  return input[0] + reduceListOne(input[1]);
 }
 
+export function reduceList(input) {
+  if (input === null) {
+    return 0;
+  }
+  return input.val + reduceList(input.next);
+}
+
+
+export function reduceTreeAcc(input) {
+  let acc = 0;
+  let nodes = [input];
+  while (nodes.length > 0) {
+    //console.log(nodes.length);
+    input = nodes.pop();
+    if (input === null) {
+      continue;
+    }
+    acc += input.val;
+    nodes.push(input.left);
+    nodes.push(input.right);
+  }
+  return acc;
+}
+
+// reduceTree(input: Tree) : number           <- typeScript
+export function reduceTree(input) {
+  if (input === null) {
+    return 0;
+  }
+  return input.val + reduceTree(input.left) + reduceTree(input.right);
+}
+
+
+export function lengthNestedArrays(input) {
+  if (input === null) {
+    return 0;
+  }
+  return 1 + lengthNestedArrays(input.next);
+}
--- src/hackerRank.test.js
+++ src/hackerRank.test.js
@@ -1,30 +1,62 @@
 import {
-repeatingString,
-sumNestedArrays
+  repeatingString,
+  reduceNestedArrays,
+  reduceListOne,
+  reduceTreeAcc,
+  lengthNestedArrays,
 } from "./hackerRank.js";
 
 test("repeatingString 0", () => {
-    var results = repeatingString("aba", 10, "a")
-    expect(results).toEqual(7);
-  });
+  var results = repeatingString("aba", 10, "a");
+  expect(results).toEqual(7);
+});
 
-  test("repeatingString 1", () => {
-    var results = repeatingString("ccc", 9, "c")
-    expect(results).toEqual(9);
-  });
-    
-  test("repeatingString 2", () => {
-    var results = repeatingString("qwerty", 19, "q")
-    expect(results).toEqual(4);
-  });
-// ---------------------------------------------------      
-  test("sumNestedArrays 0", () => {
-    var nestedArrays = [
-      [1, 3, 5, 2, 10, 238],
-      [55, 9, 3, 2],
-      [1, 9, 2, 39, 11]
-    ]
-    var results = sumNestedArrays(nestedArrays)
-    expect(results).toEqual(390);
-  });
-      
+test("repeatingString 1", () => {
+  var results = repeatingString("ccc", 9, "c");
+  expect(results).toEqual(9);
+});
+
+test("repeatingString 2", () => {
+  var results = repeatingString("qwerty", 19, "q");
+  expect(results).toEqual(4);
+});
+// ---------------------------------------------------
+
+test("reduceNestedArrays 0", () => {
+  var nestedArrays = [238, [1, 2, [[3]]], [[10, 15, 20]], [[[100]]]];
+  var results = reduceNestedArrays(nestedArrays);
+  expect(results).toEqual(389);
+});
+
+test("reduceNestedArrays 1", () => {
+  let input = [1, [2, [3, [4, null]]]]; // linked list
+  let expected = 10;
+
+  var results = reduceListOne(input);
+  expect(results).toEqual(expected);
+});
+
+// ---------------------------------------------------
+test("lengthNestedArrays 0", () => {
+  let node = function (n, next) {
+    return { val: n, next: next };
+  };
+  let input = node(1, node(2, node(3, node(56, null)))); // "list" nodes
+  let expected = 4; // length of a linked list
+  var results = lengthNestedArrays(input);
+  expect(results).toEqual(expected);
+});
+// ---------------------------------------------------
+test("reduceNestedArrays 2", () => {
+  let node = function (n, l, r) {
+    return { val: n, left: l, right: r };
+  };
+  let input = node(
+    5,
+    node(2, node(1, null, null), node(3, null, null)),
+    node(7, node(6, null, null), node(8, null, null))
+  );
+
+  var results = reduceTreeAcc(input);
+  expect(results).toEqual(32); // calculate height of tree
+});
