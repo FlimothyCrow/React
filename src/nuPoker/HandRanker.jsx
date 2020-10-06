6654a322f66a7d61bd32a62d84800dd475330235
FlimothyCrow <timshogren@gmail.com>
2020-10-06 00:51:22 -0600

    added logic for hand.length

--- src/nuPoker/HandRanker.jsx
+++ src/nuPoker/HandRanker.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import * as poker from "./nu_poker"
+import * as handEval from "./handEval"
 
 export default class HandRanker extends React.Component {
     constructor(props) {
       super(props);
       this.state = {
         currentHand : [],
+        isFullHand : false,
         deck : poker.deckMaker().map(cardObject => cardObject.face + " " + cardObject.suit)
     };
     }
@@ -13,6 +15,12 @@ export default class HandRanker extends React.Component {
     addToHand(cardObject){
         this.state.currentHand.push(cardObject)
         this.setState({currentHand : this.state.currentHand})
+    }
+
+    handSize(){
+        if (this.state.currentHand.length === 5){
+            this.setState({isFullHand : true})
+        }
     }
 
     render(){
@@ -24,13 +32,20 @@ export default class HandRanker extends React.Component {
             </ul>
             <div>
                 <h3>hand</h3>
+                {console.log(this.state.deck)}
+                <button onClick={() => this.handSize()}>Update hand size</button>
+                <body>{this.state.isFullHand ? 
+                    "true" : "false"}</body>
                 <ul>
-                    {this.state.currentHand.map(card => <li>{card}</li>)} 
+                    {this.state.currentHand.map(card => <li>{card}</li>)}
                 </ul>
             </div>
         </div>
     }
-
 }
 
+// this.state.deck is currenlty array of strings, we need array of cardObjects
 // currentHand.length === 5 => handEval(currentHand)
+// {handEval.handEval(this.state.currentHand)}
+// stop clickable cards at .length === 5
+// if .length === 5 THEN return handEval
