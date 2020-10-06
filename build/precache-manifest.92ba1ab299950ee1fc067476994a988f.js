a7353988673f731c727854ac00a737dec728d6e3
FlimothyCrow <timshogren@gmail.com>
2020-10-06 15:30:04 -0600

    static site

---
+++ build/precache-manifest.92ba1ab299950ee1fc067476994a988f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5af547eaefbbaf9f9cad000914221c57",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b28c44c68344a4259ff1",
+    "url": "/static/css/main.cc2b10a1.chunk.css"
+  },
+  {
+    "revision": "a1ef89fb68285d6e9020",
+    "url": "/static/js/2.8c18388b.chunk.js"
+  },
+  {
+    "revision": "3b173ed738774b4bb64ac2b5ddb03f3b",
+    "url": "/static/js/2.8c18388b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b28c44c68344a4259ff1",
+    "url": "/static/js/main.6c3e0f88.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  }
+]);
