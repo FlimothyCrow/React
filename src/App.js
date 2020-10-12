af68d198ebe21f7aa1c0bba36dfd235b97bc1187
FlimothyCrow <timshogren@gmail.com>
2020-10-12 09:38:44 -0600

    set up a hrefs and gameMode state data

--- src/App.js
+++ src/App.js
@@ -1,21 +1,70 @@
-import React from 'react';
-import HandRanker from './nuPoker/HandRanker'
-import './App.css';
-// npm start
-// npm test
-function App() {
+import React from "react"
+import HandRanker from "./nuPoker/HandRanker"
+import "./App.css"
+import PlayablePoker from "./playablePoker/playablePoker"
 
-  return (
-    <div className="App">
-      <header className="App-header">
-          <HandRanker></HandRanker>
-      </header>
-      
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      currentMode: "home",
+    }
+  }
+
+  setGameMode(mode) {
+    if (mode === "play") {
+      this.setState({ currentMode: "play" })
+    } else if (mode === "rank") {
+      this.setState({ currentMode: "rank" })
+    } else if (mode === "home") {
+      this.setState({ currentMode: "home" })
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div>
+          <a
+            href="play"
+            className={this.state.currentMode === "home" && "warningColor"}
+            onClick={(e) => {
+              e.preventDefault()
+              this.setGameMode("home")
+            }}
+          >
+            home
+          </a>
+          <a
+            href="play"
+            className={this.state.currentMode === "play" && "warningColor"}
+            onClick={(e) => {
+              e.preventDefault()
+              this.setGameMode("play")
+            }}
+          >
+            play
+          </a>
+          <a
+            href="rank"
+            className={this.state.currentMode === "rank" && "warningColor"}
+            onClick={(e) => {
+              e.preventDefault()
+              this.setGameMode("rank")
+            }}
+          >
+            rank
+          </a>
+        </div>
+        <header className="App-header">
+          {this.state.currentMode === "home" && <span>welcome to the new page</span>}
+          {this.state.currentMode === "rank" && <HandRanker></HandRanker>}
+          {this.state.currentMode === "play" && <PlayablePoker></PlayablePoker>}
+        </header>
+      </div>
+    )
+  }
 }
-
-export default App;
 
 // notice that the calls on 15 and 16 have different syntax
 // <FizzBuzzPrinter /> for line 16
---
+++ src/playablePoker/playablePoker.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default class PlayablePoker extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  render() {
+    return <div>hello world</div>
+  }
+}
